c4fa5558dac69f034d9c772651364874f09b6e35
Negotiv1 <sjdjdjdoqhehdjdhdlspo@gmail.com>
2025-02-18 19:05:30 +0200

    Update rightClickModule.js

--- assets /rightClickModule.js
+++ assets /rightClickModule.js
@@ -8,7 +8,7 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+function() {
     'use strict';
 
     let lastTime = 0;
@@ -38,13 +38,12 @@
     creditsDisplay.style.color = 'white';
     creditsDisplay.style.textAlign = 'center';
     creditsDisplay.style.zIndex = '9999';
-    creditsDisplay.style.background = 'linear-gradient(45deg, red, yellow, green, blue, purple)';
-    creditsDisplay.style.backgroundSize = '400% 400%';
-    creditsDisplay.style.animation = 'gradientAnimation 3s ease infinite';
+    creditsDisplay.style.background = 'black';
+    creditsDisplay.style.padding = '5px';
 
     document.body.appendChild(creditsDisplay);
 
-    creditsDisplay.innerHTML = 'By Mr.Negotiv By Ananas By Akuma | <a href="" target="_blank" style="color: white; text-decoration: underline;">.</a>';
+    creditsDisplay.innerHTML = 'By Mr.Negotiv-By Ananas-By Akuma | <a href="" target="_blank" style="color: white; text-decoration: underline;">.</a>';
 
     const fpsDisplay = document.createElement('div');
     fpsDisplay.style.position = 'absolute';
@@ -54,9 +53,8 @@
     fpsDisplay.style.color = 'white';
     fpsDisplay.style.textAlign = 'center';
     fpsDisplay.style.zIndex = '9999';
-    fpsDisplay.style.background = 'linear-gradient(45deg, red, yellow, green, blue, purple)';
-    fpsDisplay.style.backgroundSize = '400% 400%';
-    fpsDisplay.style.animation = 'gradientAnimation 3s ease infinite';
+    fpsDisplay.style.background = 'black';
+    fpsDisplay.style.padding = '5px';
 
     document.body.appendChild(fpsDisplay);
 
@@ -66,14 +64,4 @@
     }
 
     updateDisplay();
-
-    const style = document.createElement('style');
-    style.innerHTML = `
-        @keyframes gradientAnimation {
-            0% { background-position: 0% 50%; }
-            50% { background-position: 100% 50%; }
-            100% { background-position: 0% 50%; }
-        }
-    `;
-    document.head.appendChild(style);
 })();
